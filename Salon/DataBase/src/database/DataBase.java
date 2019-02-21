d61d6c0c3714d29107c343ce5ae9b6fe590559c6
Joshua Saucedo <joshuansu0897@gmail.com>
2019-02-20 20:58:29 -0700

    clase

---
+++ Salon/DataBase/src/database/DataBase.java
@@ -0,0 +1,53 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DataBase {
+
+    public static void main(String[] args) {
+
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DriverManager.getConnection("SELECT expediente, nombre, sexo, activo FROM table_name");
+
+            st = conn.createStatement();
+            rs = st.executeQuery("SELECT * FROM table_name");
+
+            System.out.println("expediente \t nombre \t sexo \t activo");
+            while (rs.next()) {
+                int expediente = rs.getInt("expediente");
+                String nombre = rs.getString("nombre");
+                String sexo = rs.getString("sexo");
+                boolean activo = rs.getBoolean("activo");
+                System.out.println(expediente + " \t " + nombre + " \t " + sexo + " \t " + activo);
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+
+                if (st != null) {
+                    st.close();
+                }
+
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println("Error: " + ex.getMessage());
+            }
+        }
+    }
+
+}
